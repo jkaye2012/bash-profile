31c91953f5fcf1d47d47bf927ed5cea4fbca3766
Jordan Kaye <jordan.kaye2@gmail.com>
2019-11-12 21:33:51 -0600

    Added first version of Python module
    Handles initial pyenv installation and detection of presence.
    Currently does not attempt to handle installation of actual Python
    versions for the user (mostly because this is so platform-dependent
    that there is a very low chance it could be solved acceptably).

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,20 @@ export BASHPROF_LIB
 # shellcheck source=lib/prelude
 . "$BASHPROF_LIB/prelude"
 
+## Provides configuration values for all modules.
+#  By importing configuration in the entrypoint, we do not have to include it in
+#  each individual module.
+
 import configuration
+
+## Individual profile modules.
+#  Each of these provide a single aspect of profile functionality.
+#  The import loads the appropriate code based upon the user's configuration.
+
+import python
+
+if bp_python_working; then
+    echo "yeah"
+else
+    echo "nope"
+fi
