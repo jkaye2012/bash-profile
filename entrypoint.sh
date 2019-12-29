43824f5da8acbfbd503485387065815ea4991722
Jordan Kaye <jordan.kaye2@gmail.com>
2019-12-29 17:58:51 -0600

    Powerline configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ BASHPROF_LIB="$BASHPROF_ROOT/lib"
 export BASHPROF_ROOT
 export BASHPROF_LIB
 
+## Import the prelude for use in all profile modules.
+
 # shellcheck source=lib/prelude
 . "$BASHPROF_LIB/prelude"
 
@@ -19,11 +21,11 @@ import configuration
 ## Individual profile modules.
 #  Each of these provide a single aspect of profile functionality.
 #  The import loads the appropriate code based upon the user's configuration.
+#  The user may also have disabled module functionality, so an import here does
+#  not mean that the module will actually take effect, only that it will be inspected.
+#  Note that the order of these may be important; for example, many modules
+#  depend on Python, which requires the "python" module to be imported before
+#  necessary functionality is enabled.
 
 import python
-
-if bp_python_working; then
-    echo "yeah"
-else
-    echo "nope"
-fi
+import powerline
